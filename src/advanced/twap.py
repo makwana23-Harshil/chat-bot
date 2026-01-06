2a1fda02b965cb689fffa47598a0392bac51d5ba
Harshil makwana <harshilmakwana2311@gmail.com>
2026-01-06 16:05:50 +0530

    Add TWAP order execution function
    Implemented TWAP function to split orders over time.

--- src/advanced/twap.py
+++ src/advanced/twap.py
@@ -1,1 +1,14 @@
 
+import time
+from src.client import BinanceFuturesClient
+
+def execute_twap(client, symbol, side, total_qty, duration_secs):
+    """TWAP implementation: Splits order into smaller chunks over time"""
+    slices = 5
+    qty_per_slice = float(total_qty) / slices
+    interval = duration_secs / slices
+    
+    for i in range(slices):
+        client.new_order(symbol=symbol, side=side, type='MARKET', quantity=qty_per_slice)
+        if i < slices - 1:
+            time.sleep(interval)
