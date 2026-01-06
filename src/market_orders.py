dee4de723c33b9056be59194bac60609cbde5523
Harshil makwana <harshilmakwana2311@gmail.com>
2026-01-06 16:08:30 +0530

    Update market_orders.py

--- src/market_orders.py
+++ src/market_orders.py
@@ -1,9 +1,8 @@
-
 import sys
 from src.client import BinanceFuturesClient
 
 if __name__ == "__main__":
-    # Example: python src/market_orders.py BTCUSDT BUY 0.01
-    symbol, side, qty = sys.argv[1], sys.argv[2], sys.argv[3]
-    client = BinanceFuturesClient(api_key="KEY", api_secret="SECRET")
-    client.new_order(symbol=symbol, side=side, type='MARKET', quantity=qty)
+    # Usage: python src/market_orders.py BTCUSDT BUY 0.01
+    client = BinanceFuturesClient()
+    res = client.new_order(sys.argv[1], sys.argv[2], 'MARKET', sys.argv[3])
+    print(f"Order Response: {res}")
