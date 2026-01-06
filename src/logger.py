a29a5a3e1e4df9b3ca0546bf18ffbcf44f18f78d
Harshil makwana <harshilmakwana2311@gmail.com>
2026-01-06 16:07:32 +0530

    Refactor logger setup to use setup_logger function

--- src/logger.py
+++ src/logger.py
@@ -1,15 +1,16 @@
 import logging
 
-def get_logger(name="BinanceBot"):
-    logger = logging.getLogger(name)
+def setup_logger():
+    logger = logging.getLogger("BinanceBot")
     logger.setLevel(logging.INFO)
     
     # File handler appends to 'bot.log' in the root directory
     fh = logging.FileHandler("bot.log")
-    # ISO-8601 format for timestamps is a professional best practice
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     
     if not logger.handlers:
         logger.addHandler(fh)
     return logger
+
+logger = setup_logger()
